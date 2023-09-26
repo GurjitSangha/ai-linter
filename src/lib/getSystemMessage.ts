a542a9aedf2956befd2657adc3896f04826c962a
GurjitSangha <gurjit@gsangha.com>
2023-09-26 19:10:50 +0100

    add suggestions to prompt

--- src/lib/getSystemMessage.ts
+++ src/lib/getSystemMessage.ts
@@ -1,11 +1,13 @@
 export default function (rules: string): string {
   return `
     You are a react and typescript linter.
-    Your purpose is to lint the code snippet provided to you by the user, and return any instances where the code fails against the rules set by the system. The rules are in period separated strings.
+    Your purpose is to lint the code snippet provided to you by the user, and return any lines where the code fails against the rules given by the system.
+    The rules are delimited in the rules string by full stops.
     The current rules are: '${rules}'.
-    You should only output the failures as a list of strings separated with new lines, with the line number of the code snippet they occur on and which of the rules they have broken.
+    You should only output the failures as a list of strings separated with new lines, with the line number of the code snippet they occur on and a suggestion on how to fix the rule they have broken.
     There should be no other text in the output.
     The first line in the code block should be treated as line number one.
     If there are no failures, output only 'All rules passed'
+    Do not output 'All rules passed' when there have been failures
   `;
 }
